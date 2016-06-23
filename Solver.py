6f0ecc4b06668695543990fb87869d6e80ce489d
alexnie01 <anie@college.harvard.edu>
2016-06-23 04:51:57 -0400

    finished linear architecture up to animation

--- Solver.py
+++ Solver.py
@@ -11,35 +11,71 @@ go = raw_input('Solver.py overwrites data in <filename>.py, which may contain da
 assert(go == 'y')
 
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import brenth
 from sympy import *
 import pdb
-from Configuration import filename, walls, dx, dp, X, P, T, k, m
+from Configuration import walls, wall_x, wall_v, k, m
 
-N = 2
-M = 2
+filename = 'quad_solver_25'
+
+N = 5
+M = 5
 
 xmin = .4
 xmax = .45
 pmin = .1
 pmax = .15
 
-def build_pos(x0, p0):
-    return (lambda t: x0 * np.cosh(np.sqrt(k/m) * t) +\
-                        p0 * np.sinh(np.sqrt(k/m) * t)/(np.sqrt(k * m)))
-def build_mom(x0, p0):
-    return (lambda t: p0 * np.cosh(np.sqrt(k/m) * t) +\
-                    np.sqrt(k * m) * x0 * np.sinh(np.sqrt(k/m) * t))    
-def step(particle, time):
+dt = .1
+
+pt_arr = np.array([])
+
+def build_pos(x0, p0, T):
+    return (lambda t: x0 * np.cosh(np.sqrt(k/m) * (t-T)) +\
+                        p0 * np.sinh(np.sqrt(k/m) * (t-T))/(np.sqrt(k * m)))
+def build_mom(x0, p0, T):
+    return (lambda t: p0 * np.cosh(np.sqrt(k/m) * (t-T)) +\
+                    np.sqrt(k * m) * x0 * np.sinh(np.sqrt(k/m) * (t-T)))    
+def step(pt_id, time):
+    particle = pt_arr[pt_id]
     timeslice = np.array([particle[0](time), particle[1](time), time])
-    pdb.set_trace()
+    if np.abs(timeslice[0]) >= np.abs(wall_x(time)):
+        # new function for root-finding:
+        f = lambda t: particle[0](t) - np.sign(particle[0](t)) * wall_x(t)
+        # numerically calculate collision time
+        t_col = brenth(f, time - dt, time, maxiter = 50)
+        # calculate collision position and momentum
+        x_col = particle[0](t_col)
+        p_col = 2 * np.sign(x_col) * wall_v(t_col) - particle[1](t_col)
+        # build new particle
+        particle = (build_pos(x_col, p_col, t_col), 
+                        build_mom(x_col, p_col, t_col), pt_id)
+        pt_arr[pt_id] = particle
+        # may cause an infinite loop?
+        return step(pt_id, time)
     return timeslice
 
 if __name__ == "__main__":
+#    pt0 = (build_pos(xmin, pmin, 0), build_mom(xmin, pmin, 0), 0)
+#    pt1 = (build_pos(xmax, pmax, 0), build_mom(xmax, pmax, 0), 1)
     pt_id = 1
-    particle_array = np.array([(build_pos(xmax, pmax), build_mom(xmax, pmax), 0)])
+    pt_arr = np.array([(build_pos(xmax, pmax, 0), 
+                        build_mom(xmax, pmax, 0), 0)])
     for x in np.arange(xmin, xmax, (xmax-xmin)/N):
         for p in np.arange(pmin, pmax, (pmax-pmin)/M):
-            particle = (build_pos(x, p), build_mom(x, p), pt_id)
+            particle = (build_pos(x, p, 0), build_mom(x, p, 0), pt_id)
             pt_id += 1
-            particle_array = np.vstack((particle_array, particle))
+            pt_arr = np.vstack((pt_arr, particle))
+    data = []
+    for pt_id in np.arange(0, N * M + 1):
+        particle = pt_arr[pt_id]
+        pt_data = np.array((particle[0](0), particle[1](0), 0))
+        for t in np.arange(dt, 1000, dt):
+            timeslice = step(pt_id, t)
+            pt_data = np.vstack((pt_data, timeslice))
+        data.append(pt_data)
+    data = np.array(data)
+    np.save(filename, data)
+
         
