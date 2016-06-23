4ad25d02496f1e788de614906b84ee24e2759f80
alexnie01 <anie@college.harvard.edu>
2016-06-23 03:18:48 -0400

    started working on linear architecture for Solver.py

--- Solver.py
+++ Solver.py
@@ -13,10 +13,10 @@ assert(go == 'y')
 import numpy as np
 from sympy import *
 import pdb
-from Configuration import filename, walls, dx, dp, X, P, T
+from Configuration import filename, walls, dx, dp, X, P, T, k, m
 
-N = 4
-M = 4
+N = 2
+M = 2
 
 xmin = .4
 xmax = .45
@@ -24,16 +24,22 @@ pmin = .1
 pmax = .15
 
 def build_pos(x0, p0):
-    position = x0 * cosh(np.sqrt(k/m)*T) +\
-    p0 * sinh(np.sqrt(k/m) * T)/(np.sqrt(k * m))
-    return (lambda t: position.subs(T, t))
+    return (lambda t: x0 * np.cosh(np.sqrt(k/m) * t) +\
+                        p0 * np.sinh(np.sqrt(k/m) * t)/(np.sqrt(k * m)))
 def build_mom(x0, p0):
-    momentum = p0 * cosh(np.sqrt(k/m)*T) +\
-    np.sqrt(k * m) * x0 * sinh(np.sqrt(k/m) * T)
-    return (lambda t: position.subs(T, t))
-    
-def step(time):
-    pass
+    return (lambda t: p0 * np.cosh(np.sqrt(k/m) * t) +\
+                    np.sqrt(k * m) * x0 * np.sinh(np.sqrt(k/m) * t))    
+def step(particle, time):
+    timeslice = np.array([particle[0](time), particle[1](time), time])
+    pdb.set_trace()
+    return timeslice
 
 if __name__ == "__main__":
-    print "hey!"
+    pt_id = 1
+    particle_array = np.array([(build_pos(xmax, pmax), build_mom(xmax, pmax), 0)])
+    for x in np.arange(xmin, xmax, (xmax-xmin)/N):
+        for p in np.arange(pmin, pmax, (pmax-pmin)/M):
+            particle = (build_pos(x, p), build_mom(x, p), pt_id)
+            pt_id += 1
+            particle_array = np.vstack((particle_array, particle))
+        
