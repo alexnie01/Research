8bdc8061035a83521f10d81df987fa06ceccb299
alexnie01 <anie@college.harvard.edu>
2016-06-25 15:16:04 -0400

    debugging Solver

--- Solver.py
+++ Solver.py
@@ -12,22 +12,22 @@ assert(go == 'y')
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import brenth
+from scipy.optimize import brenth, newton
 from sympy import *
 import pdb
 from Configuration import walls, wall_x, wall_v, k, m
 
-filename = 'quad_solver_25'
+filename = 'quad_solver_1000_fast'
 
-N = 5
-M = 5
+N = 50
+M = 20
 
 xmin = .4
-xmax = .45
-pmin = .1
-pmax = .15
+xmax = .405
+pmin = .005
+pmax = .01
 
-dt = .1
+dt = .01
 
 pt_arr = np.array([])
 
@@ -37,23 +37,31 @@ def build_pos(x0, p0, T):
 def build_mom(x0, p0, T):
     return (lambda t: p0 * np.cosh(np.sqrt(k/m) * (t-T)) +\
                     np.sqrt(k * m) * x0 * np.sinh(np.sqrt(k/m) * (t-T)))    
-def step(pt_id, time):
+def step(pt_id, time, repeat = False):
     particle = pt_arr[pt_id]
     timeslice = np.array([particle[0](time), particle[1](time), time])
     if np.abs(timeslice[0]) >= np.abs(wall_x(time)):
         # new function for root-finding:
         f = lambda t: particle[0](t) - np.sign(particle[0](t)) * wall_x(t)
         # numerically calculate collision time
-        t_col = brenth(f, time - dt, time, maxiter = 50)
+        t_col = brenth(f, time - dt, time)
+        if repeat:
+            try:
+                t_col1 = brenth(f, time - dt, t_col)
+            except:
+                pass
         # calculate collision position and momentum
         x_col = particle[0](t_col)
-        p_col = 2 * np.sign(x_col) * wall_v(t_col) - particle[1](t_col)
+        p_col = 2 * m * np.sign(x_col) * wall_v(t_col) - particle[1](t_col)
         # build new particle
-        particle = (build_pos(x_col, p_col, t_col), 
+        new_particle = (build_pos(x_col, p_col, t_col), 
                         build_mom(x_col, p_col, t_col), pt_id)
-        pt_arr[pt_id] = particle
+        if repeat:
+            pdb.set_trace()
+            new
+        pt_arr[pt_id] = new_particle
         # may cause an infinite loop?
-        return step(pt_id, time)
+        return step(pt_id, time, True)
     return timeslice
 
 if __name__ == "__main__":
@@ -71,11 +79,11 @@ if __name__ == "__main__":
     for pt_id in np.arange(0, N * M + 1):
         particle = pt_arr[pt_id]
         pt_data = np.array((particle[0](0), particle[1](0), 0))
-        for t in np.arange(dt, 1000, dt):
+        for t in np.arange(dt, 150, dt):
             timeslice = step(pt_id, t)
             pt_data = np.vstack((pt_data, timeslice))
         data.append(pt_data)
     data = np.array(data)
     np.save(filename, data)
-
+    data = None
         
